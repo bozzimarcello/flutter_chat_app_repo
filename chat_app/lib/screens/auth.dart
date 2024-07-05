9686927b5905b717af45129f3a70a398ea0265cb
Marcello Bozzi <bozzimarcello@gmail.com>
2024-07-05 15:42:15 +0200

    button added

--- chat_app/lib/screens/auth.dart
+++ chat_app/lib/screens/auth.dart
@@ -13,6 +13,8 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  var _isLogin = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,6 +57,26 @@ class _AuthScreenState extends State<AuthScreen> {
                               ),
                             obscureText: true,
                           ),
+                          const SizedBox(height: 12),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+                            ),
+                            child: Text(_isLogin 
+                              ? 'Login' 
+                              : 'SignUp'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                setState(() {
+                                    _isLogin = !_isLogin;
+                                });
+                              }, 
+                              child: Text( _isLogin 
+                                ? 'Create an account' 
+                                : 'I already have an account.'),
+                              ),
                         ],
                       ),
                     ),
