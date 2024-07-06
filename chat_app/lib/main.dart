c1058837ac8b6800001ccc5cf52b1dc369158717
Marcello Bozzi <bozzimarcello@gmail.com>
2024-07-06 15:17:29 +0200

    firebase config added

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:chat_app/screens/auth.dart';
 
-void main() {
+// C:\Users\Utente\AppData\Local\Pub\Cache\bin
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
