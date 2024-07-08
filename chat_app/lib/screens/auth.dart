05b0a3ba9006e12e6629eea26f3e430671a98a5c
Marcello Bozzi <bozzimarcello@gmail.com>
2024-07-08 11:32:19 +0200

    user data saved on firestore added

--- chat_app/lib/screens/auth.dart
+++ chat_app/lib/screens/auth.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:chat_app/widgets/user_image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-
-import 'package:chat_app/widgets/user_image_picker.dart';
 
 final _firebase = FirebaseAuth.instance;  // instance of FirebaseAuth
 
@@ -66,6 +66,16 @@ class _AuthScreenState extends State<AuthScreen> {
         await storageRef.putFile(_selectedImage!);
         final imageUrl = await storageRef.getDownloadURL();
         print(imageUrl); // to be removed in the final version
+
+        await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userCredentials.user!.uid)
+          .set({
+            'username': 'to be done...',
+            'email': _enteredEmail,
+            'image_url': imageUrl,
+          });
+
       }
     } on FirebaseAuthException catch (error) {
         if (error.code == 'email-already-in-use') {
