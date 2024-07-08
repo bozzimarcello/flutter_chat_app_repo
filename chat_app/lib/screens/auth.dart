22d1e1c5c98716197d86991a7b5baf42916472b7
Marcello Bozzi <bozzimarcello@gmail.com>
2024-07-08 10:20:27 +0200

    image picker added

--- chat_app/lib/screens/auth.dart
+++ chat_app/lib/screens/auth.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import 'package:chat_app/widgets/user_image_picker.dart';
+
 final _firebase = FirebaseAuth.instance;  // instance of FirebaseAuth
 
 // AuthScreen is a StatefulWidget, meaning that it can change its state
@@ -88,6 +90,8 @@ class _AuthScreenState extends State<AuthScreen> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
+                          if (!_isLogin)
+                            UserImagePicker(),
                           TextFormField(
                             decoration: const InputDecoration(
                               labelText: 'Email Address',
